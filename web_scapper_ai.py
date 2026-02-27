2a7fba55ec024e7ca6581c8eef3d3b42d800298c
wayzer2318 <arezki.ait.kettout@gmail.com>
2026-02-27 18:05:04 +0100

    adding a web scrapper powered by ai

---
+++ web_scapper_ai.py
@@ -0,0 +1,48 @@
+from os import write
+import re
+import requests
+from bs4 import BeautifulSoup
+import streamlit as sl
+from langchain_ollama import OllamaLLM
+
+# load a model
+llm = OllamaLLM(model="mistral")
+
+# web scrapper func
+
+
+def web_scrapper(url):
+    try:
+        sl.write(f"scrapping website :{url}")
+        headers = {"user-agent": "chrome"}
+        response = requests.get(url, headers=headers)
+        if response.status_code != 200:
+            return f"fail to fetch {url}"
+        soup = BeautifulSoup(response.text, "html.parser")
+        paragraphs = soup.find_all("p")
+        text = " ".join([p.get_text() for p in paragraphs])
+
+        return text[:2000]
+    except Exception as e:
+        return f"error: {str(e)}"
+
+
+# ai summerize func
+def summerize_content(content):
+    sl.write("summerizing ...")
+    return llm.invoke(f"summerize the following content\n\n{content[:1000]}")
+
+
+# web UI
+sl.title("ai web scrapper")
+sl.write("enter url")
+url = sl.text_input("url :")
+
+if url:
+    content = web_scrapper(url)
+    if "failed" in content or "stop" in content:
+        sl.write(content)
+    else:
+        summary = summerize_content(content)
+        sl.subheader("website summary")
+        sl.write(summary)
